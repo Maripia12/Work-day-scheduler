e6b224a0cb8bf01b951dbf463460ea72f4614863
Pia Paulino <maripia1220@gmail.com>
2021-06-25 14:43:35 -0400

    Updating

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Work-day-scheduler
+
+## Objective:
+
+Create a calender app that allows the user to save events for each hour of the day.  The app will feature HTML, CSS powered by jQuery.
+
+## Details:
+* The current day at the top displays at the top of the calendar. 
+
+* When the user scrolls is presented with timeblocks. The timeblocks will be color coded to indicate wether it is in the past, present, or future.
+
+* 
+
+
--- script.js
+++ script.js
@@ -41,8 +41,17 @@ $(".saveBtn").on("click", function () {
 
 } 
 
-// timeColor();
+timeColor(); {
 
-// $("#9 .description").val(localStorage.getItem("9"));
-// $("#5 .description").val(localStorage.getItem("5"));
-// // }
+$("#9 .description").val(localStorage.getItem("9"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#1 .description").val(localStorage.getItem("1"));
+$("#2 .description").val(localStorage.getItem("2"));
+$("#3 .description").val(localStorage.getItem("3"));
+$("#4 .description").val(localStorage.getItem("4"));
+$("#5 .description").val(localStorage.getItem("5"));
+
+
+}
